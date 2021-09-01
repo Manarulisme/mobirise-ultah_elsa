678fec85f791109cb4a326a37c79da2b9849faa6
Manarul Huda <manarulhuda57@gmail.com>
2021-09-01 16:40:57 +0700

    File projek ultah elsa

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ulang tahun Elsa</title>
+    <link rel="stylesheet" href="asset/css/bootstrap.min.css">
+    <script src="asset/jquery/jquery.min.js"></script>
+    <script src="asset/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<div class="container-fluid">
+    <h1 id="text-nowrap">Selamat Ulang Tahun Sayang</h1>
+</div>
+ 
+</body>
+</html>
